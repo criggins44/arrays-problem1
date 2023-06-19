9af31b1490996ae588c2481c78adcef1e0004e22
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-19 16:26:29 -0400

    added curly braces to the statement to fix the error

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-let myArray = ['Georgia', 'Florida', 'New York', 'New Jersey', 'California', 'Arizona', 'Illinois', 'Missouri']
-let secondArray = ['America', 'Mexico', 'Canada', 'France', 'Italy', 'Germany', 'England']
-
-console.log(myArray.slice(0, 2));
-
-console.log(myArray.length);
+let myArray = ['Georgia', 'Florida', 'Alabama', 'Texas', 'Tennessee', 'Virginia']
+let myTeams = ['Bulldogs', 'Hurricanes', 'Wildcats', 'Hornets', 'Aggies', 'Lakers']
 
 const getArray = function (array, num) {
 
-if(num > array.length); 
-	return "enter a shorter value";
+	if (num < 0){return "try again"};
+	if (num > array.length){return "choose a smaller value"};
 
-return array.slice(0, num);
+	return array.slice(0, num);
 }
 
-console.log(getArray(myArray, 2));
+console.log(getArray(myTeams, 5));
