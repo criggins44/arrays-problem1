e8ba685a75fa0d0391120d11232e3530c5c24d87
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-19 16:44:34 -0400

    added comments to explain logic

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ let myArray = ['Georgia', 'Florida', 'Alabama', 'Texas', 'Tennessee', 'Virginia'
 let myTeams = ['Bulldogs', 'Hurricanes', 'Wildcats', 'Hornets', 'Aggies', 'Lakers']
 
 const getArray = function (array, num) {
+//variable that accepts the parameters of the function
 
-	if (num < 0){return "try again"};
+	if (num < 0){return "enter a larger number value"};
+//check to ensure the number larger than the first index place
+
 	if (num > array.length){return "choose a smaller value"};
-
+//check to ensure the number is not larger than the number of items in the array
+	
 	return array.slice(0, num);
+//returns the portion of the array within the given parameters
 }
 
 console.log(getArray(myTeams, 5));
